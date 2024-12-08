081e4a7108c6c438256a68c88fbbca65e3439718
LokeshVaddepally77 <vaddepallylokesh362@gmail.com>
2024-12-08 00:49:36 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/React_Final_Project/',
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: '/index.html'
+      }
+    }
+  }
 })
