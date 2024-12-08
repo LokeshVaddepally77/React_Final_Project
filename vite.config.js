6b7ca4d38425569149f8c81519293eab9fcf2f79
LokeshVaddepally77 <vaddepallylokesh362@gmail.com>
2024-12-07 20:40:49 -0800

    Add gh-pages configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/React_Final_Project/', 
+  plugins: [react()]
 })
