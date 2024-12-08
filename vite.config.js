1a6ebfd6fdac244b147ba473994d1504ae0fcc69
LokeshVaddepally77 <vaddepallylokesh362@gmail.com>
2024-12-07 21:44:20 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React_Final_Project/',  // This should match your repository name exactly
+  base: '/React_Final_Project/',
   plugins: [react()]
 })
